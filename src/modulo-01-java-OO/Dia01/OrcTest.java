6d28d4d3d81b8f5149754ea36f2e285138a419d4
unknown <0152067@207V-22.acad.feevale.br>
2015-04-06 14:14:46 -0300

    orc teste 2

--- src/modulo-01-java-OO/Dia01/OrcTest.java
+++ src/modulo-01-java-OO/Dia01/OrcTest.java
@@ -28,6 +28,75 @@ public class OrcTest
     
     @Test
     public void orcRecebeAtaque() {
+        
+         // Arrange - Montagem dos dados de teste
+        Orc umOrc;
+        Elfo umElfo;
+        // Act - Execução da ação de testes
+        umOrc = new Orc();
+        umElfo= new Elfo("legolas");
+        // Assert - Verificação
+        int esperado = 100;
+        umElfo.atirarFlecha(umOrc);
+        int resultadoObtido = umOrc.getVida();
+        
+        assertEquals(esperado, resultadoObtido);
+        
+    }
+    
+    
+    @Test
+    public void orcRecebeAtaqueMedio() {
+        
+         // Arrange - Montagem dos dados de teste
+        Orc umOrc;
+        Elfo umElfo;
+        // Act - Execução da ação de testes
+        umOrc = new Orc();
+        umElfo= new Elfo("legolas");
+        // Assert - Verificação
+        int esperado = 50;
+       
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        int resultadoObtido = umOrc.getVida();
+        
+        assertEquals(esperado, resultadoObtido);
+        
+    }
+    
+    @Test
+    public void orcRecebeAtaqueGrande() {
+        
+         // Arrange - Montagem dos dados de teste
+        Orc umOrc;
+        Elfo umElfo;
+        int i = 0;
+        // Act - Execução da ação de testes
+        umOrc = new Orc();
+        umElfo= new Elfo("legolas");
+        // Assert - Verificação
+        int esperado = 0;
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        umElfo.atirarFlecha(umOrc);
+        
+        int resultadoObtido = umOrc.getVida();
+        
+        assertEquals(esperado, resultadoObtido);
+        
     }
 }
 
