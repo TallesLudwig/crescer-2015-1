4b27d84165b079ca81e9ed26d805ee5c06d65f06
Talles Ludwig <tallesludwig@gmail.com>
2015-04-14 17:33:39 -0300

    ExercitoDeElfos

---
+++ src/modulo-01-java-OO/Dia01/ExercitoDeElfos.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/**
+ * Write a description of class ExercitoDeElfos here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ExercitoDeElfos
+{
+    private HashMap<String, Elfo> Exercito= new HashMap<>();  
+    
+    public HashMap<String, Elfo> getExercito(){
+        return this.Exercito;
+    }
+    
+    public void alistar(Elfo elfoNovo){
+       boolean podeAlistar=  elfoNovo instanceof ElfoVerde ||  elfoNovo instanceof ElfoNoturno ;
+      if(podeAlistar){
+         Exercito.put(elfoNovo.getNome(), elfoNovo);      
+           
+      }  
+   }
+   
+   
+   public Elfo buscar(String nome){
+       return Exercito.get(nome);
+    }
+}
+
+
+//recebe um staus por parametro e agrupa esses
