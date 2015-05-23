33011c3f20d3f6da55a1620e78d36dc8509cadd2
Marlon Bernardes <marlonjb.cb@gmail.com>
2015-05-22 21:46:11 -0300

    feat(dia07): adicionar aplicação de exemplo feita com spring-mvc

---
+++ src/modulo-04-java/dia07/filmator-web/src/main/java/filmator/dao/FilmeDao.java
@@ -0,0 +1,20 @@
+package filmator.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import filmator.model.Filme;
+
+public class FilmeDao {
+
+	private static List<Filme> filmesSalvos = new ArrayList<Filme>();
+	
+	public List<Filme> buscaTodosFilmes(){
+		return filmesSalvos;
+	}
+
+	public void salvar(Filme filme) {
+		filmesSalvos.add(filme);
+	}
+}
+ 
---
+++ src/modulo-04-java/dia07/filmator-web/src/main/java/filmator/model/Filme.java
@@ -0,0 +1,29 @@
+package filmator.model;
+
+public class Filme {
+
+	private String nome;
+	private Genero genero;
+	
+	public Filme(){
+	}
+
+	public Filme(String nome){
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	public Genero getGenero() {
+		return genero;
+	}
+}
